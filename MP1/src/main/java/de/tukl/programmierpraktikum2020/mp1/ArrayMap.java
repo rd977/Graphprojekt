1dde8dff50deaf7ef98e7aa306483fca24a1d984
omarhumeedat <omarhumeedat@outlook.com>
2020-05-09 19:27:59 +0200

    Aufgabe 2

---
+++ MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/ArrayMap.java
@@ -0,0 +1,100 @@
+package de.tukl.programmierpraktikum2020.mp1;
+
+import java.security.Key;
+
+public class ArrayMap implements Map<String,Integer>{
+    public String [] keys;
+    public Integer [] values;
+    int size;
+    public ArrayMap(){
+        keys = new String[5];
+        values = new Integer[5];
+        size = 0;
+    }
+
+
+    @Override
+    public Integer get(String key) {
+        for(int i = 0 ; i < size ; i++){
+            if(keys[i].equals(key)){
+                return values[i];
+            }
+        }
+        return null;
+    }
+    public int get_Indix(String key){         // Hilfsfunktion , gibt uns die Indize der Elemnte in dem Array
+        for(int i = 0 ; i < size ; i++){
+            if(keys[i] ==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+        public void inc_dic_reaseSize(int n) {    /// Hilfsgunktion -> ändert den Length von dem Array bei Fügen und Löschen . n ist skalar
+            String[] copy_Keys = new String[keys.length*n];
+            Integer[] copy_Values = new Integer[values.length*n];
+            for (int i = 0; i < keys.length; i++){
+                copy_Keys[i] = keys[i];
+                copy_Values[i] = values[i];
+            }
+            keys = copy_Keys;
+            values = copy_Values;
+
+        }
+
+
+
+
+
+    @Override
+    public void put(String key, Integer value) {
+
+        int indix = get_Indix(key);
+       if(size == keys.length &&indix == -1){     // Array Length inkriminieren wenn es kein genug Platz in Array gibt(die Doppelte)
+           inc_dic_reaseSize(2);
+        }
+        if(indix == -1){          // falls der Key nicht vorgekomen ist
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
+        else{
+            values[indix]=value;  // falls der Key  schon im Array ist, hier änderen wir nur den Value
+        }
+    }
+
+    @Override
+    public void remove(String key) {
+        int indix = get_Indix(key);
+        if(indix!=-1 && keys[indix] == key ){       // wenn der Key ist im Array
+                for(int j = indix; j < keys.length - 1; j++){  //  elemente nach links verschieben
+                    keys[j] = keys[j+1];
+                    values[j] = values[j+1];
+
+                }
+                size--;
+            }
+        if(size < (keys.length/2)){     // Hier um wenige Platz in RAM zu reservieren , diskriminieren wir die Arrays
+            inc_dic_reaseSize(1);
+
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void keys(String[] array) {
+        if (array == null|| array.length< size()) {
+            throw new IllegalArgumentException();
+        } else {
+            for(int i = 0 ; i < size ; i++) {
+                array[i] = keys[i];
+            }
+        }
+    }
+
+}
