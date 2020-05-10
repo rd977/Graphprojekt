d8d76f5fd679f8c57c339cda8acb3a41d972fe88
omarhumeedat <omarhumeedat@outlook.com>
2020-05-10 18:43:06 +0200

    Aufgabe 2 -Verbesserung Platzoptimierung

---
+++ MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/Pair.java
@@ -0,0 +1,28 @@
+package de.tukl.programmierpraktikum2020.mp1;
+
+public class Pair<String,Integer> {
+    private String fst; //first member of pair
+    private Integer sec; //second member of pair
+
+    public Pair(String fst, Integer sec) {
+        this.fst = fst;
+        this.sec = sec;
+    }
+
+    public void setFirst(String fst) {
+        this.fst = fst;
+    }
+
+    public void setSecond(Integer sec) {
+        this.sec = sec;
+    }
+
+    public String getFirst() {
+        return fst;
+    }
+
+    public Integer getSecond() {
+        return sec;
+    }
+}
+
