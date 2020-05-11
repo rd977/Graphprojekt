2b2f704bb5d380e8686e0f3ee38ce3082b03c37d
Jakob Dreßler <jakob.dressler@yahoo.de>
2020-05-11 15:16:04 +0200

    Aufgabe 4

--- MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/BibleAnalyzer.java
+++ MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/BibleAnalyzer.java
@@ -1,17 +1,52 @@
 package de.tukl.programmierpraktikum2020.mp1;
 
+import java.lang.reflect.Array;
 import java.util.Comparator;
 
 public class BibleAnalyzer {
     public static void countWords(Map<String, Integer> counts) {
-        throw new RuntimeException("TODO");
+        String[] words = new String[counts.size()];
+        counts.keys(words);
+        for (String word : words) {
+            int count = 0;
+            for (String w : Util.getBibleWords()) {
+                if( w.equals(word) ) {
+                    count++;
+                }
+            }
+            counts.put( word, count );
+        }
     }
 
     public static void main(String[] args) {
-        throw new RuntimeException("TODO");
+        Map<String, Integer> WordCountMap = new ArrayMap();
+        WordCountMap.put("Gott", null);                                    //fülle die Map mit Beispielwörtern.
+        WordCountMap.put("Vater", null);
+        WordCountMap.put("Amen", null);
+        WordCountMap.put("Gott", null);
+        WordCountMap.put("Bundeskanzler", null);
+        WordCountMap.put("Teufel", null);
+        WordCountMap.put("Abraham", null);
+        WordCountMap.put("Jakob", null);
+        WordCountMap.put("Jesus", null);
+        countWords(WordCountMap);                                       //Zähle wie oft jedes Beispielwort in der Bibel vorkommt und schreibe es in die Map.
+        String[] words = new String[WordCountMap.size()];
+        WordCountMap.keys(words);                                       //Schreibe die Wörter(keys) aus der Map in ein Array.
+        sort(words, WordCountMap);                                      //Sortiere das Array absteigend nach der Häufigkeit des jeweiligen Wortes in der Bibel
+        for ( String word : words ){
+            System.out.println(WordCountMap.get(word) + " " + word);
+        }
     }
 
-    public static void sort(String[] words, Map<String, Integer> counts) {
-        throw new RuntimeException("TODO");
+    public static void sort(String[] words, Map<String, Integer> counts) {      //Bubblesort verwendet (Code-Inspiration: javabeginners.de)
+        String helpString;
+        for (int i = 0; i < words.length - 1; i++) {
+            if ( counts.get(words[i]) < counts.get(words[i + 1]) ) {
+                helpString = words[i];
+                words[i] = words[i + 1];
+                words[i + 1] = helpString;
+                sort( words, counts );
+            }
+        }
     }
 }
--- MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/Node.java
+++ MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/Node.java
@@ -4,7 +4,5 @@ package de.tukl.programmierpraktikum2020.mp1;
 public class Node<K, V>  {
         public  K key;
         public  V value;
-
         public  Node<K,V> next;
-
 }
