210f7eb4bbb3217ca55c547481e393a20ef15414
omar humeedat <omar-humeedat@hotmail.com>
2020-05-11 04:54:21 +0200

    Upload New File

---
+++ MP1/omarTmp/TreeNode.java
@@ -0,0 +1,13 @@
+package de.tukl.programmierpraktikum2020.mp1;
+
+public class TreeNode<K,V> {
+    K key;
+    V value;
+    TreeNode<K,V> left , right;
+    TreeNode(K key , V value){
+        this.key=key;
+        this.value=value;
+        left=null;
+        right=null;
+    }
+}
