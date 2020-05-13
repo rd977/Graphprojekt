dd1ab0ca0ba8f83ad2bdd011c50ef5bd48530c62
omarhumeedat <omarhumeedat@outlook.com>
2020-05-13 16:37:41 +0200

    Aufgabe 4 - last version

--- MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/BibleAnalyzer.java
+++ MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/BibleAnalyzer.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 
 public class BibleAnalyzer {
     public static void countWords(Map<String, Integer> counts) { ;
+
     for (String word : Util. getBibleWords ()) {
         Integer v = counts.get(word);
         //um (null +1) zu vermeiden , überprüfen wir ersmal den value
@@ -17,13 +18,17 @@ public class BibleAnalyzer {
         TreeMap<String, Integer> map = new TreeMap<>(Comparator.<String>naturalOrder());
         countWords(map);
         String[] words = new String[map.size()];
+
         //alle Wötrter in Array words speichern
         map.keys(words);
+
         // Array words aufsteigent sortieren
         sort(words, map);
+
         //print alle Wörter aud Console
         for (String s : words){
             System.out.println(map.get(s) + " " + s);
+
     }
 
 
@@ -31,11 +36,11 @@ public class BibleAnalyzer {
     //insertion sort
     public static void sort(String[] words, Map<String, Integer> counts) {
             String temp;
-            for (int i = 1; i <words.length; i++) {
+            for (int i = 0; i < words.length; i++) {
                 temp = words[i];
                 int j = i;
-                Integer d = counts.get(temp);
-                while (j > 0 && counts.get(words[j - 1] )>d) {
+                Integer value = counts.get(temp);
+                while (j > 0 && counts.get(words[j - 1] )>value) {
                     words[j] = words[j - 1];
                     j--;
                 }
