8c93fcd9d91b16ef123cc6acb28b841a51a8f976
omarhumeedat <omarhumeedat@outlook.com>
2020-05-13 11:35:28 +0200

    Aufgabe 4 - 1 test faild

--- MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/BibleAnalyzer.java
+++ MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/BibleAnalyzer.java
@@ -4,57 +4,36 @@ import java.lang.reflect.Array;
 import java.util.Comparator;
 
 public class BibleAnalyzer {
-    public static void countWords(Map<String, Integer> counts) {
-        String[] words = new String[counts.size()];
-        counts.keys(words);
-        for (String word : words) {
-            int count = 0;
-            for (String w : Util.getBibleWords()) {
-                if( w.equals(word) ) {
-                    count++;
-                }
-            }
-            counts.put( word, count );
-        }
+    public static void countWords(Map<String, Integer> counts) { ;
+    for (String word : Util. getBibleWords ()) {
+        Integer v = counts.get(word);
+        counts.put(word , (v== null)? 1 :v+1) ;
     }
+    }
+
 
     public static void main(String[] args) {
-        Map<String, Integer> WordCountMap = new ArrayMap();
-        WordCountMap.put("Gott", null);                                    //fülle die Map mit Beispielwörtern.
-        WordCountMap.put("Vater", null);
-        WordCountMap.put("Amen", null);
-        WordCountMap.put("Gott", null);
-        WordCountMap.put("Bundeskanzler", null);
-        WordCountMap.put("Teufel", null);
-        WordCountMap.put("Abraham", null);
-        WordCountMap.put("Jakob", null);
-        WordCountMap.put("Amen", null);
-        WordCountMap.put("Engel", null);
-        WordCountMap.put("Gottes", null);
-        WordCountMap.put("Gebote", null);
-        WordCountMap.put("Israel", null);
-        WordCountMap.put("Herr", null);
-        WordCountMap.put("aber", null);
-        WordCountMap.put("da", null);
-        WordCountMap.put("der", null);
-        countWords(WordCountMap);                                       //Zähle wie oft jedes Beispielwort in der Bibel vorkommt und schreibe es in die Map.
-        String[] words = new String[WordCountMap.size()];
-        WordCountMap.keys(words);                                       //Schreibe die Wörter(keys) aus der Map in ein Array.
-        sort(words, WordCountMap);                                      //Sortiere das Array absteigend nach der Häufigkeit des jeweiligen Wortes in der Bibel
-        for ( String word : words ){
-            System.out.println(WordCountMap.get(word) + " " + word);
-        }
+        TreeMap<String,Integer> m = new TreeMap<>(Comparator.<String>naturalOrder());
+        countWords(m);
+        String[] words = new String[m.size()];
+        m.keys(words);
+        sort(words , m);
+
+
     }
 
-    public static void sort(String[] words, Map<String, Integer> counts) {      //Bubblesort verwendet (Code-Inspiration: javabeginners.de)
-        String helpString;
-        for (int i = 0; i < words.length - 1; i++) {
-            if ( counts.get(words[i]) < counts.get(words[i + 1]) ) {
-                helpString = words[i];
-                words[i] = words[i + 1];
-                words[i + 1] = helpString;
-                sort( words, counts );
+    public static void sort(String[] words, Map<String, Integer> counts) {
+            String temp;
+            for (int i = 1; i <words.length; i++) {
+                temp = words[i];
+                int j = i;
+                Integer d = counts.get(temp);
+                while (j > 0 && counts.get(words[j - 1] )>d) {
+                    words[j] = words[j - 1];
+                    j--;
+                }
+                words[j] = temp;
             }
-        }
+
     }
 }
