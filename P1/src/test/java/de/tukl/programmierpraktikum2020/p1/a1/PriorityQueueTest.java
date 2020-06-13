0f75a478fd60fa5d273337e3124c3020f6cd9fa6
omar <omar-humeedat@hotmail.com>
2020-06-13 15:26:45 +0200

    Aufgabe 1b

--- P1/src/test/java/de/tukl/programmierpraktikum2020/p1/a1/PriorityQueueTest.java
+++ P1/src/test/java/de/tukl/programmierpraktikum2020/p1/a1/PriorityQueueTest.java
@@ -3,11 +3,10 @@ package de.tukl.programmierpraktikum2020.p1.a1;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PriorityQueueTest {
     /**
