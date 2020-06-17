b6883ccf7fb2ef3e5f2dee20af7f4961a28cbce6
omar <omar-humeedat@hotmail.com>
2020-06-17 15:04:24 +0200

    Aufgabe 1d Alle Teile

--- P1/src/main/java/de/tukl/programmierpraktikum2020/p1/a1/FibNode.java
+++ P1/src/main/java/de/tukl/programmierpraktikum2020/p1/a1/FibNode.java
@@ -1,16 +1,20 @@
 package de.tukl.programmierpraktikum2020.p1.a1;
 
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class FibNode<E>{
 
     E key;
-    FibNode<E> parent , prev , next , child;
+    FibNode<E> parent;
+    LinkedList<FibNode<E>> child;
     int degree;
     boolean marked ;
+
     FibNode(E key){
         this.key=key;
+        child = new LinkedList<>();
     }
 
 }
--- P1/src/main/java/de/tukl/programmierpraktikum2020/p1/a1/FibonacciHeap.java
+++ P1/src/main/java/de/tukl/programmierpraktikum2020/p1/a1/FibonacciHeap.java
@@ -6,7 +6,11 @@ import java.util.zip.CheckedOutputStream;
 
 public class FibonacciHeap<E> implements PriorityQueue<E>{
     FibNode<E> maxNode;
+    FibNode<E> foundNode;
+    FibNode<E> vater;
+    int size =0;
     Comparator<E> comp;
+    boolean found = false;
     LinkedList<FibNode<E>> rootlist = new LinkedList<>();
 
 
@@ -14,178 +18,24 @@ public class FibonacciHeap<E> implements PriorityQueue<E>{
     public FibonacciHeap(Comparator<E> comp) {
         this.comp = comp;
     }
-    public  void fixnode(FibNode<E> f, E element){
-        System.out.println("we entered fixnode");
-        System.out.println(element);
-
-        if (f.child!=null) {
-            FibNode<E> c = f.child;
-            FibNode<E> c1 = f.child;
-            boolean b = false;
-            while (c != null) {
-                if (comp.compare(c.key, element) > 0) {
-                    b = true;
-                    break;
-                }
-                c = c.next;
-            }
-            if (!b) {
-                f.key = element;
-                if (comp.compare(f.key, maxNode.key) > 0)
-                    maxNode = f;
-
-
-            } else
-
-                while (c1 != null) {
-                    c1.parent = null;
-                    c1.prev = null;
-                    c1.next = null;
-                    rootlist.add(c1);
-                    c1 = c1.next;
-                }
-            f.child = null;
-            f.key = element;
-            f.degree = 0;
-
-        }
-        else {
-            f.key = element;
-            if (comp.compare(f.key, maxNode.key) > 0){
-                maxNode = f;}
-        }
-    }
-    public void toroot(FibNode<E> f){
-        f.parent.degree-=1;
-        if (f.prev!=null){
-        f.prev.next=f.next;}
-        f.marked=false;
-        f.prev=null;
-        f.next=null;
-        rootlist.add(f);
-
-
-        if (f.parent.marked==true){
-            f.parent.marked=false;
-            toroot(f.parent);}
-        else{
-            f.parent.marked=true;}
-        }
-
-    public boolean search(LinkedList<FibNode<E>> rootlist,E element,E element1){
-        for (FibNode<E> f:rootlist) {
-            if (comp.compare(f.key, element) == 0) {
-                if (f.parent!=null){
-                    f.parent.degree-=1;}
-                if (f.prev!=null){
-                f.prev.next=f.next;}
-                f.prev=null;
-                f.next=null;
-                this.rootlist.add(f);
-                fixnode(this.rootlist.getLast(),element1);
-                if (f.parent!=null){
-                    if (f.parent.marked==true)
-                        toroot(f.parent);
-                    else
-                        f.parent.marked=true;
-                    f.parent=null;
-
-                }
-
-
-
-                return true;
-
-            }
-            else if (comp.compare(f.key,element) > 0) {
-                if (f.child!=null) {
-                    LinkedList<FibNode<E>> L = new LinkedList<>();
-                    FibNode<E> c = f.child;
-                    while (c != null) {
-                        L.add(c);
-                        c = c.next;
-
-                    }
-                    if (search(L, element,element1) == true)
-                        return (true);
-                }
-            }
-        }
 
 
 
 
 
-
-        return false;
-    }
-    //###############InsertNode########
+//---------------insert------------------------------------------
+// --------------insertNord-------------
+// Hilft beim Einfügen von childern in rootlist
     private void insertNode(FibNode<E> node) {
-        if(rootlist.isEmpty()){
-            maxNode = node;
-        }
-        if(comp.compare(maxNode.key, node.key) < 0){
-            maxNode = node;
-        }
-        rootlist.add(node);
-    }
+         if(rootlist.isEmpty()){
+        maxNode = node;
+           }
+    if(comp.compare(maxNode.key, node.key) < 0){
+        maxNode = node;
+         }
+    rootlist.add(node);
+}
 
-    //############## Binomial ############
-    private FibNode<E> Binomial() {
-
-        HashMap<Integer, FibNode<E>> degRoots = new HashMap<Integer, FibNode<E>>();
-        if(rootlist.isEmpty()) {
-            return null;
-        }
-        FibNode<E> max = rootlist.get( 0 );
-
-        int i = 0;
-
-        while (rootlist.size() > i) {
-                if(comp.compare(max.key,rootlist.get(i).key)<0){
-                    max = rootlist.get(i);
-                }
-
-            if (!degRoots.containsKey( rootlist.get(i).degree )) {
-                degRoots.put(rootlist.get(i).degree , rootlist.get(i));
-                i++;
-            } else {
-                int tempDegree = rootlist.get(i).degree;
-                FibNode gleichDeg = degRoots.get(tempDegree);
-                FibNode newTree = UnionZweiBäume(rootlist.get(i) , gleichDeg);
-
-
-                rootlist.set(i, newTree);
-                i = rootlist.indexOf(gleichDeg) > i ? i : i - 1;
-                rootlist.remove(gleichDeg);
-                degRoots.remove(tempDegree);
-            }
-        }
-        return max;
-    }
-
-    private FibNode<E> UnionZweiBäume(FibNode<E> FN1, FibNode<E> FN2) {
-
-        if (comp.compare(FN1.key, FN2.key)>0) {
-                FN2.next = FN1.child;
-                if (FN1.child != null)
-                    FN1.child.prev = FN2;
-                FN1.child = FN2 ;
-                FN2.prev = null;
-                FN2.parent = FN1;
-                FN1.degree++;
-                return FN1;
-            } else {
-                FN1.next = FN2.child;
-                if (FN2.child != null){
-                    FN2.child.prev = FN1;
-                }
-                    FN2.child = FN1 ;
-                  FN1.prev = null;
-                  FN1.parent = FN2;
-                  FN2.degree++;
-            return FN2;
-    }}
     @Override
     public void insert(E elem) {
         FibNode<E> node = new FibNode<E>(elem);
@@ -196,42 +46,84 @@ public class FibonacciHeap<E> implements PriorityQueue<E>{
             maxNode = node;
         }
         rootlist.add(node);
+        size++;
 
     }
 
 
-
+//------------------------------merge--------------------------------------
     @Override
     public void merge(PriorityQueue otherQueue) {
        while (!((FibonacciHeap<E>) otherQueue).rootlist.isEmpty()){
            insertNode(((FibonacciHeap<E>) otherQueue).rootlist.remove());
        }
-        System.out.println(otherQueue.deleteMax());
-
     }
 
 
+//------------------------deletMax----------------------------------------------------------
+    //------------------------DELETMAX------------------------------------------------------
 
+    //----------------- Binomoial ----------------
+    // alle Bäume die gleichen Degree haben , werden zusammengesetzt
+    //gibt auch den neuen Max zurück
+    private FibNode<E> Binomial() {
+        HashMap<Integer, FibNode<E>> degRoots = new HashMap<Integer, FibNode<E>>();
+        if(rootlist.isEmpty()) {
+            return null;
+        }
+
+        FibNode<E> max = rootlist.get( 0 );
+        int i = 0;
+
+        while (rootlist.size() > i) {
+            if(comp.compare(max.key,rootlist.get(i).key)<0){
+                max = rootlist.get(i);
+            }
+            if (!degRoots.containsKey( rootlist.get(i).degree )) {
+                degRoots.put(rootlist.get(i).degree , rootlist.get(i));
+                i++;
+            } else {
+                int tempDegree = rootlist.get(i).degree;
+                FibNode gleichDeg = degRoots.get(tempDegree);
+                FibNode newTree = mergTowtrees(rootlist.get(i) , gleichDeg);
+
+                rootlist.set(i, newTree);
+                i = rootlist.indexOf(gleichDeg) > i ? i : i - 1;
+                rootlist.remove(gleichDeg);
+                degRoots.remove(tempDegree);
+            }
+        }
+        return max;
+    }
+
+    //------------------ mergTowtrees------------------------------
+    // Hilfsfunktion bei Binomial Funktion
+    private FibNode<E> mergTowtrees(FibNode<E> FN1, FibNode<E> FN2) {
+        if (comp.compare(FN1.key, FN2.key)>0) {
+            FN1.child.add(FN2);
+            FN2.parent = FN1;
+            FN1.degree++;
+            return FN1;
+        } else {
+            FN2.child.add(FN1);
+            FN1.parent = FN2;
+            FN2.degree++;
+            return FN2;
+        }}
     @Override
     public E deleteMax() {
         if(rootlist.isEmpty()){
             return null;
-
         }else {
-            FibNode<E> temp = maxNode.child;
-            while (temp != null) {
-                temp.parent = null;
-                temp.marked = false;
-                insertNode(temp);
-                temp = temp.next;
-                if (temp != null && temp.prev != null)
-                    temp.prev.next = null;
-            }
 
-            FibNode<E> n = maxNode;
+            while(!maxNode.child.isEmpty()) {
+                insertNode(maxNode.child.remove());
+            }
+            FibNode<E> max = maxNode;
             rootlist.remove(maxNode);
             maxNode = Binomial();
-            return n.key;
+            size--;
+            return max.key;
         }
         }
 
@@ -249,51 +141,116 @@ public class FibonacciHeap<E> implements PriorityQueue<E>{
         return rootlist.isEmpty();
     }
 
+    //------------------------------------update-----------------------------------------------------
+    //------------------------------------UPDATE-----------------------------------------------------
+
+    //-----------HilfsFunktionBeimSuchen
+    private void findeNode(FibNode<E> node, E elem) {
+        if(comp.compare(node.key , elem) == 0 ){
+            foundNode = node ;
+            vater = node.parent;
+        }
+        else if(comp.compare(node.key , elem) > 0){
+            for(int i = 0 ; i<node.degree ; i++){
+                findeNode(node.child.get(i) , elem);
+            }
+        }
+    }
+
+    //-------------- SearchNachElement----------------------
+    private FibNode<E> find (E elem){
+        foundNode = null;
+        vater=null;
+        for (int i = 0 ; i <rootlist.size() ; i++){
+            if(foundNode!=null){
+                break;
+            }
+            findeNode(rootlist.get(i) , elem);
+        }
+        return foundNode;
+    }
+
+    private void updateElem(FibNode<E> node, E updateElem) {
+        node.key = updateElem;
+        FibNode<E> Vater = node.parent;
+        if (Vater != null && comp.compare(node.key, Vater.key) < 0) {
+            cut(node, Vater);
+            fix_after_cut(Vater);
+        }
+        if (comp.compare(node.key, maxNode.key) > 0)
+            maxNode = node;
+    }
+
+    private void cut(FibNode<E> child, FibNode<E> Vater) {
+        Vater.degree--;
+        child.parent=null;
+        child.marked = false;
+        int i = Vater.child.indexOf(child);
+        insertNode(Vater.child.remove(i));
+    }
+    private void fix_after_cut(FibNode<E> y) {
+        FibNode<E> z = y.parent;
+        if (z != null) {
+            if (y.marked == false)
+                y.marked=true;
+            else {
+                cut(y, z);
+                fix_after_cut(z);
+            }
+        }
+    }
     @Override
     public boolean update(E elem, E updatedElem) {
-        boolean a=(search(rootlist,elem,updatedElem));
-         return(a);
+        FibNode<E> x = find(elem);
+        if (x != null) {
+            updateElem(x, updatedElem);
+            return true;
+        }
 
-
+         return false;
 
 
     }
 
 
-
-    @Override
-    public void map(UnaryOperator f) {
-        FibonacciHeap<E> temp = new FibonacciHeap<E>( comp );
-        while(!(rootlist.isEmpty())){
-            temp.insert((E)f.apply(rootlist.removeFirst().key));
+//---------------------map------------------------------------------
+    private void mapNode(FibNode<E> node ,UnaryOperator<E> f ) {
+        update(node.key ,(E)f.apply(node.key) );
+        for (int i = 0; i < node.degree; i++) {
+            mapNode(node.child.get(i), f);
         }
-        while(!temp.isEmpty()){
-            insert( temp.deleteMax() );
-        }
-    
     }
-    public static void main(String[] args){
-        FibonacciHeap<Integer> f = new FibonacciHeap<>(Comparator.<Integer>naturalOrder());
+    public void map(UnaryOperator<E> f) {
+        for(int i = 0 ; i < rootlist.size();i++){
+            mapNode(rootlist.get(i) , f);
+        }
+
+    }
+
+
+   /* public static void main(String[] args){
+        FibonacciHeap<Integer> g = new FibonacciHeap<>(Comparator.<Integer>naturalOrder());
         FibonacciHeap<Integer> x = new FibonacciHeap<>(Comparator.<Integer>naturalOrder());
-        f.insert(2);
-        f.insert(3);
-        UnaryOperator<Integer> x2 = a -> a * 2;
-        f.map(x2);
-        System.out.println("f Max = " + f.deleteMax());
-        /*f.insert(0);
-        f.insert(5);
+      //  f.insert(2);
+     //   f.insert(3);
+       // UnaryOperator<Integer> x2 = a -> a * 2;
+       // f.map(x2);
+       // System.out.println("f Max = " + f.deleteMax());
+        g.insert(8);
+        g.insert(6);
        x.insert(1);
-        x.insert(7);
-        x.insert(5);
         x.insert(3);
-        f.update(3,4);
-        f.merge(x);
+        x.insert(2);
+        x.insert(5);
+        x.insert(6);
+        x.merge(g);
+       // x.map(c->c*2);
+       // System.out.println(x.deleteMax());
 
-        System.out.println(f.deleteMax());
-        System.out.println(f.max());
-        System.out.println(x.isEmpty());
-        FibNode<Integer> a=new FibNode(5);
-        System.out.println(a.next!=null);*/
-    }
+      //System.out.println(x.rootlist.size());
+       // System.out.println(x.max());
+      //  FibNode<Integer> a=new FibNode(5);
+      //  System.out.println(a.next!=null);
+    }*/
 
 }
